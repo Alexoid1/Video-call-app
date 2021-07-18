f78999f3f65c713b5dcb104ed33ed039e8b85f2b
alexoid <alex_zam@hotmail.es>
2021-07-18 18:38:24 -0500

    add styles to home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -17,4 +17,11 @@ module HomeHelper
         current_user.online? ? "home/partials/nav/dropdown/gofflinebtn" :
         "home/partials/nav/dropdown/gonlinebtn"
     end
+    def btnStart
+        if user_signed_in?
+        "home/partials/startbtn/scrollbtn"
+        else
+        "home/partials/startbtn/authbtn"
+        end
+    end
 end
